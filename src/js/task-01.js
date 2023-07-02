2ba59bf07d26af9adfc4872b18723936cc289414
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-07-02 20:14:02 +0300

    update

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,8 @@
-const qtyOfCategories = document.querySelectorAll(".item");
+const qtyOfCategories = document.querySelectorAll("li.item");
 console.log(`Number of categories: ${qtyOfCategories.length}`);
+
+const categoryName = document.querySelectorAll("h2");
+categoryName.forEach((element) => {
+  console.log(`Category: ${element.textContent}`);
+  console.log(`Elements: ${element.nextElementSibling.children.length}`);
+});
