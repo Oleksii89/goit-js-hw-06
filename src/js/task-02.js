15231ff9c1f78d0c502298650c6e3c7ffdd61b5f
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-07-03 15:51:19 +0300

    update

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+ingredients.forEach((element) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = element;
+  ingredient.classList.add("item");
+  ingredientsRef.append(ingredient);
+});
